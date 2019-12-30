fd1f365c55731458ed4724672a6687bb3eaed505
Zayts3v <tiago-pinha@hotmail.com>
2019-12-30 17:39:57 +0000

    Merge de commits.

--- Tutorial 1/CannyDetector.m
+++ Tutorial 1/CannyDetector.m
@@ -1,6 +1,6 @@
 % Script para > Design a program to detect edges using Canny Detector
 
-image = imread('Input2.png','png');
+image = imread('Lena_Gaussian_0-025.png','png');
 
 [I,I2,I3] = main_CannyDetector(image);
 
